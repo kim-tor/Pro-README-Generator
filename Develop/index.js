cca89e2f9bcbe2a19fe5fc945fbcb5fc9c1237b4
Kim Torres <kimberley.5118@gmail.com>
2020-10-26 16:37:58 -0400

    create question array

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,53 @@
+const inquirer = require("inquirer");
+
+
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email adress"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project's name?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please write a short description of your project."
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "What kind of license should your project have?"
+    },
+    {
+        type: "input",
+        name: "install",
+        message: "What command should be run to install dependencies?"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "What command should be run to run tests ?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What does the user need to know about using the repo?"
+    },
+    {
+        type: "input",
+        name: "contributions",
+        message: "What does the user need to know about contributing to the repo?"
+    },
 ];
 
 // function to write README file
